c1a891212a8c6a00d336f30c4f95f48a41f36462
Victor Oliveira <victor.rsoliveira@gmail.com>
2017-05-19 01:54:38 -0300

    Sending data to ThingSpeak, but code is a mess

--- examples/sim800-app/main.c
+++ examples/sim800-app/main.c
@@ -18,27 +18,24 @@
 #define SIM_UART  UART_DEV(1)
 #define BAUDRATE (9600U)
 #define THREAD1_STACKSIZE   (THREAD_STACKSIZE_MAIN)
-// #define THREAD2_STACKSIZE   (THREAD_STACKSIZE_MAIN)
+#define THREAD2_STACKSIZE   (THREAD_STACKSIZE_MAIN)
 // #define THREAD3_STACKSIZE   (THREAD_STACKSIZE_MAIN)
 
 static char thread1_stack[THREAD1_STACKSIZE];
-// static char thread2_stack[THREAD2_STACKSIZE];
+static char thread2_stack[THREAD2_STACKSIZE];
 // static char thread3_stack[THREAD3_STACKSIZE];
 
 static kernel_pid_t thread1_pid;
-// static kernel_pid_t thread2_pid;
+static kernel_pid_t thread2_pid;
 // static kernel_pid_t thread3_pid;
 // static kernel_pid_t main_thread_pid;
 
 // Data sent to SIM800L module for HTTP connection _start
-const char *data1 = "AT+SAPBR=1,1\r\n";
-const char *data2 = "AT+HTTPINIT\r\n";
-const char *data3 = "AT+HTTPPARA=\"CID\",1\r\n";
-char data4[256];
-const char *data5 = "AT+HTTPACTION=0\r\n";
-const char *data6 = "AT+HTTPREAD\r\n";
-const char *data7 = "AT+HTTPTERM\r\n";
-const char *data8 = "AT+SAPBR=0,1\r\n";
+uint8_t counter = 0;
+char cRcv = 0;
+const char *data[] = {"AT+SAPBR=1,1\r\n","AT+HTTPINIT\r\n","AT+HTTPPARA=\"CID\",1\r\n",
+                    "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/update?api_key=TGGXXSHNNX87LYX7&field1=%d\"\r\n",
+"AT+HTTPACTION=0\r\n","AT+HTTPREAD\r\n","AT+HTTPTERM\r\n","AT+SAPBR=0,1\r\n"};
 
 // Button and LED pins
 gpio_t buttonpin = ARDUINO_PIN_2;
@@ -52,7 +49,7 @@ uint8_t field_value;
 void isr_button(void *arg){
   (void)arg;
   msg_t msg;
-  thread_wakeup(thread1_pid);
+  // thread_wakeup(thread1_pid);
   msg_send(&msg,thread1_pid);
   return;
 }
@@ -63,78 +60,71 @@ void *thread_handler(void *arg){
   msg_t msg;
   while (1) {
     msg_receive(&msg);
-    // Gets current system time
-    time_struct = xtimer_now();
-    field_value = time_struct.ticks32 % 100;
-    snprintf(data4, sizeof(data4), "AT+HTTPPARA=\"URL\",\"api.thingspeak.com/update?api_key=TGGXXSHNNX87LYX7&field1=%d\"\r\n", field_value);
-    // Toggles the built-in LED
-    gpio_toggle(ledpin);
-    printf("Thread1 is executing... Data to be sent: %d\n\n", field_value);
-    // AT+SAPBR=1,1
-    uart_write(SIM_UART, (uint8_t *)data1,strlen(data1)); //strlen((char *)data)
-    xtimer_sleep(5);
-    // AT+HTTPINIT
-    uart_write(SIM_UART, (uint8_t *)data2,strlen(data2)); //strlen((char *)data)
-    xtimer_sleep(1);
-    // AT+HTTPPARA="CID",1
-    uart_write(SIM_UART, (uint8_t *)data3,strlen(data3)); //strlen((char *)data)
-    xtimer_sleep(1);
-    // AT+HTTPPARA="URL",<addr>
-    uart_write(SIM_UART, (uint8_t *)data4,strlen(data4)); //strlen((char *)data)
-    xtimer_sleep(1);
-    // AT+HTTPACTION=0
-    uart_write(SIM_UART, (uint8_t *)data5,strlen(data5)); //strlen((char *)data)
-    xtimer_sleep(5);
-    // AT+HTTPREAD
-    uart_write(SIM_UART, (uint8_t *)data6,strlen(data6)); //strlen((char *)data)
-    xtimer_sleep(1);
-    // AT+HTTPTERM
-    uart_write(SIM_UART, (uint8_t *)data7,strlen(data7)); //strlen((char *)data)
-    xtimer_sleep(1);
-    // AT+SAPBR=0,1
-    uart_write(SIM_UART, (uint8_t *)data8,strlen(data8)); //strlen((char *)data)
-    xtimer_sleep(1);
-    printf("Data sent!!!! The thread 1 is not running anymore.\n\n");
+    if (counter == 3) {
+      char data4[100];
+      // Gets current system time
+      time_struct = xtimer_now();
+      field_value = time_struct.ticks32 % 100;
+      snprintf(data4, sizeof(data4), data[counter], field_value);
+      printf("Data to be sent: %d\n", field_value);
+      uart_write(SIM_UART, (uint8_t *)data4,strlen(data4)); //strlen((char *)data)
+    }
+    else {
+      uart_write(SIM_UART, (uint8_t *)data[counter],strlen(data[counter])); //strlen((char *)data)
+    }
+    printf("State: %d\n", counter);
   }
   return NULL;
 }
 
-// void *thread2_handler(void *arg){
-//   (void)arg;
-//   msg_t msg;
-//   while (1) {
-//     msg_receive(&msg);
-//     printf("Thread2 will send data through uart...\n");
-//     uart_write(SIM_UART, (uint8_t *)data2,strlen(data2)); //strlen((char *)data)
-//     xtimer_usleep(1000000U);
-//     printf("Data sent!!!! The thread 2 is not running anymore.\n\n");
-//     xtimer_usleep(5000000U);
-//     thread_wakeup(thread3_pid);
-//     msg_send(&msg,thread3_pid);
-//   }
-//   return NULL;
-// }
+void *thread2_handler(void *arg){
+  (void)arg;
+  msg_t msg;
+  uint8_t resp = 0;
+  // uint8_t state = 0;
+  // uint8_t get = 0;
+  // char str[50] = {0};
+  // uint32_t ind = 0;
+  while (1) {
+    msg_receive(&msg);
 
-// void *thread3_handler(void *arg){
-//   (void)arg;
-//   msg_t msg;
-//   while (1) {
-//     msg_receive(&msg);
-//     printf("Thread3 will send data through uart...\n");
-//     uart_write(SIM_UART, (uint8_t *)data2,strlen(data2)); //strlen((char *)data)
-//     xtimer_usleep(1000000U);
-//     printf("Data sent!!!! The thread 3 is not running anymore.\n\n");
-//     xtimer_usleep(5000000U);
-//     msg_send(&msg,main_thread_pid);
-//   }
-//   return NULL;
-// }
+    if (msg.content.value == 'O' /* capital letter 'o' */ && resp == 0) {
+      resp = 1;
+      printf("O");
+    }
+    else if (msg.content.value == 'K' /* capital letter 'K' */ && resp == 1) {
+      // printf("resp: %d", resp);
+      if (counter != 4){
+        resp = 0;
+        printf("K!\n");
+        counter = (counter + 1) % 8;
+        if (counter != 0)
+        msg_send(&msg,thread1_pid);
+      } else {
+        printf("K!\n");
+        resp = 2;
+      }
+    } else if (msg.content.value == '+' && resp == 2) {
+      resp = 0;
+      printf("ACTION!\n");
+      counter = (counter + 1) % 8;
+      msg_send(&msg,thread1_pid);
+    } else if (resp != 2) {
+      resp = 0;
+      printf("%c", (char) msg.content.value);
+    }
+  }
+  return NULL;
+}
 
 static void rx_cb(void *uart, uint8_t c){
   msg_t msg;
   msg.type = (int)uart;
   msg.content.value = (uint32_t)c;
-  printf("%c",(char)msg.content.value);
+  cRcv = c;
+  thread_wakeup(thread2_pid);
+  msg_send(&msg,thread2_pid);
+  // printf("%c",(char)msg.content.value);
 }
 
 int main(void)
@@ -157,7 +147,7 @@ int main(void)
     printf("GPIO 13 initialized successfully!\n");
   else printf("GPIO 13: INITIALIZATION ERROR!");
   // victorrsolivera - Code_end
-  // thread2_pid = thread_create(thread2_stack, THREAD2_STACKSIZE,THREAD_PRIORITY_MAIN - 1,THREAD_CREATE_SLEEPING,thread2_handler,NULL, "thread2");
+  thread2_pid = thread_create(thread2_stack, THREAD2_STACKSIZE,THREAD_PRIORITY_MAIN - 1,THREAD_CREATE_SLEEPING,thread2_handler,NULL, "thread2");
   // thread3_pid = thread_create(thread3_stack, THREAD3_STACKSIZE,THREAD_PRIORITY_MAIN - 3,THREAD_CREATE_SLEEPING,thread3_handler,NULL, "thread3");
 
 
